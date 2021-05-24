c1746fee614e162dcc603565a6745a2e915d5c18
Zak <zak.zangrando@outlook.com>
2021-05-24 14:14:55 +0200

    Refactoring of views inside each component, validation refactoring

--- src/app/components/cadastro.js
+++ src/app/components/cadastro.js
@@ -33,33 +33,16 @@ export default function Cadastro({ addNewPerson }) {
       setRedirect(true);
     }
   };
-  ///
-  return (
-    <>
+  /// views
+  const Header = () => {
+    return (
       <h2 className="text-2xl p-2 mt-2  font-bold leading-7 text-gray-900 sm:text-3xl sm:truncate">
         Novo Cadastro
       </h2>
-      {/* message */}
-      <div
-        className="bg-red-lightest border border-red-light text-red-dark pl-4 pr-8 py-3 rounded relative"
-        role="alert"
-      >
-        <span className="block sm:inline">
-          <span className="absolute pin-t pin-b pin-r pr-2 py-3">
-            <svg
-              className="h-6 w-6 text-red"
-              role="button"
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 20 20"
-            >
-              <title>Close</title>
-              {/* <path d="M14.348 14.849a1.2 1.2 0 0 1-1.697 0L10 11.819l-2.651 3.029a1.2 1.2 0 1 1-1.697-1.697l2.758-3.15-2.759-3.152a1.2 1.2 0 1 1 1.697-1.697L10 8.183l2.651-3.031a1.2 1.2 0 1 1 1.697 1.697l-2.758 3.152 2.758 3.15a1.2 1.2 0 0 1 0 1.698z" /> */}
-              <XIcon />
-            </svg>
-          </span>
-          {displayMessage}
-        </span>
-      </div>
+    );
+  };
+  const Form = () => {
+    return (
       <div className="bg-gray-50">
         <div className="hidden sm:block" aria-hidden="true">
           <div className="py-5">
@@ -239,7 +222,39 @@ export default function Cadastro({ addNewPerson }) {
           </div>
         </div>
       </div>
+    );
+  };
 
+  const Messages = () => {
+    return (
+      <div
+        className="bg-red-lightest border border-red-light text-red-dark pl-4 pr-8 py-3 rounded relative"
+        role="alert"
+      >
+        <span className="block sm:inline">
+          <span className="absolute pin-t pin-b pin-r pr-2 py-3">
+            <svg
+              className="h-6 w-6 text-red"
+              role="button"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 20 20"
+            >
+              <title>Close</title>
+              {/* <path d="M14.348 14.849a1.2 1.2 0 0 1-1.697 0L10 11.819l-2.651 3.029a1.2 1.2 0 1 1-1.697-1.697l2.758-3.15-2.759-3.152a1.2 1.2 0 1 1 1.697-1.697L10 8.183l2.651-3.031a1.2 1.2 0 1 1 1.697 1.697l-2.758 3.152 2.758 3.15a1.2 1.2 0 0 1 0 1.698z" /> */}
+              <XIcon />
+            </svg>
+          </span>
+          {displayMessage}
+        </span>
+      </div>
+    );
+  };
+  ///
+  return (
+    <>
+      <Header />
+      <Messages />
+      <Form />
       {redirect ? <Redirect push to="/" /> : null}
     </>
   );
--- src/app/components/table.js
+++ src/app/components/table.js
@@ -2,100 +2,24 @@ import { useState } from "react";
 import { ChevronLeftIcon, ChevronRightIcon } from "@heroicons/react/solid";
 
 export default function Table(props) {
-  const [list, setList] = useState(props[0]);
+  const [list] = useState(props[0]);
   const [curPage, setCurPage] = useState(1);
   const [pageBegin, setPageBegin] = useState(0);
   const [pageEnd, setPageEnd] = useState(3);
+  const [numberOfPages] = useState(Math.round(list.length / 3));
   const [people] = useState(list.slice(pageBegin, pageEnd));
-  console.log("hhh", people.slice(pageBegin, pageEnd));
-  return (
-    <>
-      {/* Table */}
-      <h2 className="text-2xl font-bold leading-7 p-2 mt-2 text-gray-900 sm:text-3xl sm:truncate">
-        Visualizar Cadastrados
-      </h2>
-      <div className="flex flex-col pt-20 bg-gray-50">
-        <div className="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
-          <div className="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
-            <div className="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
-              <table className="min-w-full divide-y divide-gray-200">
-                <thead className="bg-gray-50">
-                  <tr>
-                    <th
-                      scope="col"
-                      className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
-                    >
-                      Nome
-                    </th>
-                    <th
-                      scope="col"
-                      className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
-                    >
-                      Idade
-                    </th>
-                    <th
-                      scope="col"
-                      className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
-                    >
-                      Estado Civil
-                    </th>
-                    <th
-                      scope="col"
-                      className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
-                    >
-                      CPF
-                    </th>
-                    <th
-                      scope="col"
-                      className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
-                    >
-                      Cidade
-                    </th>
-                    <th
-                      scope="col"
-                      className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
-                    >
-                      Estado
-                    </th>
-                  </tr>
-                </thead>
-                <tbody className="bg-white divide-y divide-gray-200">
-                  {people.map((person) => (
-                    <tr key={person.cpf}>
-                      <td className="px-6 py-4 whitespace-nowrap">
-                        <div className="text-sm text-gray-900">
-                          {person.nome}
-                        </div>
-                      </td>
-                      <td className="px-6 py-4 whitespace-nowrap">
-                        <div className="text-sm text-gray-900">
-                          {person.idade}
-                        </div>
-                        <div className="text-sm text-gray-500">
-                          {person.department}
-                        </div>
-                      </td>
-                      <td className="px-6 py-4 whitespace-nowrap">
-                        {person.estadoCivil}
-                      </td>
-                      <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
-                        {person.cpf}
-                      </td>
-                      <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
-                        {person.cidade}
-                      </td>
-                      <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
-                        {person.estado}
-                      </td>
-                    </tr>
-                  ))}
-                </tbody>
-              </table>
-            </div>
-          </div>
-        </div>
-      </div>
-      {/* pagination */}
+  console.log("-->", people, numberOfPages);
+
+  // views
+  const Pages = () => {
+    return (
+      <p className="z-10 bg-indigo-50 border-indigo-500 text-indigo-600 relative inline-flex items-center px-4 py-2 border text-sm font-medium">
+        a
+      </p>
+    );
+  };
+  const Pagination = () => {
+    return (
       <div className="bg-white px-4 py-3 flex items-center justify-between border-t border-gray-200 sm:px-6">
         <div className="flex-1 flex justify-between sm:hidden">
           <a
@@ -114,9 +38,9 @@ export default function Table(props) {
         <div className="hidden sm:flex-1 sm:flex sm:items-center sm:justify-between">
           <div>
             <p className="text-sm text-gray-700">
-              Showing <span className="font-medium">1</span> to{" "}
-              <span className="font-medium">10</span> of{" "}
-              <span className="font-medium">97</span> results
+              Showing <span className="font-medium">{pageBegin + 1}</span> to{" "}
+              <span className="font-medium">{pageEnd}</span> of{" "}
+              <span className="font-medium">{list.length}</span> results
             </p>
           </div>
           <div>
@@ -132,6 +56,7 @@ export default function Table(props) {
                 <ChevronLeftIcon className="h-5 w-5" aria-hidden="true" />
               </a>
               {/* Current: "z-10 bg-indigo-50 border-indigo-500 text-indigo-600", Default: "bg-white border-gray-300 text-gray-500 hover:bg-gray-50" */}
+
               <a
                 href="#"
                 aria-current="page"
@@ -139,12 +64,7 @@ export default function Table(props) {
               >
                 1
               </a>
-              <a
-                href="#"
-                className="bg-white border-gray-300 text-gray-500 hover:bg-gray-50 relative inline-flex items-center px-4 py-2 border text-sm font-medium"
-              >
-                2
-              </a>
+              <Pages />
               <a
                 href="#"
                 className="relative inline-flex items-center px-2 py-2 rounded-r-md border border-gray-300 bg-white text-sm font-medium text-gray-500 hover:bg-gray-50"
@@ -156,6 +76,102 @@ export default function Table(props) {
           </div>
         </div>
       </div>
+    );
+  };
+  const Table = () => {
+    return (
+      <>
+        <h2 className="text-2xl font-bold leading-7 p-2 mt-2 text-gray-900 sm:text-3xl sm:truncate">
+          Visualizar Cadastrados
+        </h2>
+        <div className="flex flex-col pt-20 bg-gray-50">
+          <div className="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+            <div className="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
+              <div className="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
+                <table className="min-w-full divide-y divide-gray-200">
+                  <thead className="bg-gray-50">
+                    <tr>
+                      <th
+                        scope="col"
+                        className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                      >
+                        Nome
+                      </th>
+                      <th
+                        scope="col"
+                        className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                      >
+                        Idade
+                      </th>
+                      <th
+                        scope="col"
+                        className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                      >
+                        Estado Civil
+                      </th>
+                      <th
+                        scope="col"
+                        className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                      >
+                        CPF
+                      </th>
+                      <th
+                        scope="col"
+                        className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                      >
+                        Cidade
+                      </th>
+                      <th
+                        scope="col"
+                        className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                      >
+                        Estado
+                      </th>
+                    </tr>
+                  </thead>
+                  <tbody className="bg-white divide-y divide-gray-200">
+                    {people.map((person) => (
+                      <tr key={person.cpf}>
+                        <td className="px-6 py-4 whitespace-nowrap">
+                          <div className="text-sm text-gray-900">
+                            {person.nome}
+                          </div>
+                        </td>
+                        <td className="px-6 py-4 whitespace-nowrap">
+                          <div className="text-sm text-gray-900">
+                            {person.idade}
+                          </div>
+                          <div className="text-sm text-gray-500">
+                            {person.department}
+                          </div>
+                        </td>
+                        <td className="px-6 py-4 whitespace-nowrap">
+                          {person.estadoCivil}
+                        </td>
+                        <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+                          {person.cpf}
+                        </td>
+                        <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+                          {person.cidade}
+                        </td>
+                        <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+                          {person.estado}
+                        </td>
+                      </tr>
+                    ))}
+                  </tbody>
+                </table>
+              </div>
+            </div>
+          </div>
+        </div>
+      </>
+    );
+  };
+  return (
+    <>
+      <Table />
+      <Pagination />
     </>
   );
 }
